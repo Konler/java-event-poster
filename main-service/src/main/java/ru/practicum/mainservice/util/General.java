2f988e902e608632c1f5194fc5273c9dc5953604
Konler <yulya201195@mail.ru>
2023-08-08 21:07:51 +0300

    2 этап (080823)

--- main-service/src/main/java/ru/practicum/mainservice/util/General.java
+++ main-service/src/main/java/ru/practicum/mainservice/util/General.java
@@ -17,7 +17,7 @@ public class General {
     public static final String MIN_TIME = "2000-01-01 00:00:00";
 
     public static final String MAX_TIME = "3000-01-01 00:00:00";
-    
+
     public static PageRequest toPage(Integer from, Integer size, Sort sort) {
         return PageRequest.of(from > 0 ? from / size : 0, size, sort);
     }
